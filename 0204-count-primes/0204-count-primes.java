94b302deb1f5cb5b78c73ea191315722ffba01fa
Ridhamgupta <250263720+ridhamguptaprogramming-ops@users.noreply.github.com>
2026-06-28 16:41:18 +0530

    Time: 89 ms (88.34%), Space: 51 MB (85.94%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int countPrimes(int n) {
+        if (n <= 2) {
+            return 0;
+        }
+
+        boolean[] composite = new boolean[n];
+
+        int limit = (int) Math.sqrt(n);
+
+        for (int i = 2; i <= limit; i++) {
+            if (!composite[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    composite[j] = true;
+                }
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 2; i < n; i++) {
+            if (!composite[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
